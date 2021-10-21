cc9f8b6ce5ee753a8b6d57ade9ea9fb2f735b59b
magdakam <magdalenakaminska1981@gmail.com>
2021-10-21 15:05:05 +0200

    urls added to show index on localhost

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url("", views.index, name="index"),
+    url(r'^$', views.index, name="index"),
     url("index", views.index, name="index"),
 ]
