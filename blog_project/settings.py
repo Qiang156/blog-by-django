bc80fa7bb2d51c7932e900e6b6bcf6fdbd68cf21
Jenson-Mac <Jenson.zq@gmail.com>
2021-10-21 12:01:49 +0200

    change project name

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'blog-project.urls'
+ROOT_URLCONF = 'blog_project.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'blog-project.wsgi.application'
+WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 
 # Database
