f2d56851b6a6be7b3a7e4d6dc76fe8e2f2b3da26
yali zhang <yali.zhang.2019@gmail.com>
2021-10-22 16:08:04 +0200

    the style of post block

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from blog.models import Post
+
 
 def index(request):
-    context = { "just_testing" : "Hello!" }
-    return render(request, "blog/index.html", context)
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+   
 
