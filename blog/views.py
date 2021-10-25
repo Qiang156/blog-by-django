52a910bf538c953cda913a638cd369e134733bcd
magdakam <magdalenakaminska1981@gmail.com>
2021-10-25 07:11:43 +0200

    views for viewing post list and post detail

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,11 @@ def index(request):
     context = { "posts" : posts }
     return render(request, "blog/index.html", context)
 
+def post_list(request):
+    list_of_posts = Post.objects.order_by("created_at")
+    context = { "posts" : list_of_posts }
+    return render(request, 'blog/post_list.html', context)
+    
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
