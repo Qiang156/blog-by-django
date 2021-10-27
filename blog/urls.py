0befa57b4181387994ff0b09205df112261fe479
rrcchirinosc <rrchirinosc@gmail.com>
2021-10-27 11:10:47 +0200

    redirect not logged in user to the login page for current functions that require to be logged in such as Edit

--- blog/urls.py
+++ blog/urls.py
@@ -19,8 +19,8 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)', views.post_detail, name='post_detail'),
     url(r'^getPost/(?P<post_id>\d+)', views.getPost, name="getPost"),
     
-    url(r'^register/$', views.register, name='register'),
-    url(r'^login/$', views.user_login, name="login"),
-    url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^register/', views.register, name='register'),
+    url(r'^login/', views.user_login, name="login"),
+    url(r'^logout/', views.user_logout, name='logout'),
 ]
 
